6e7e9559e28ddf593c75bd1b3958b5e590c330c8
riko <riko@balak.it>
2024-02-17 06:10:30 -0600

    this library looks very good with a barebones setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo allServos = Servo();
 
 #define LED D7
+#define PWM_RED D10
+#define PWM_YELLOW D8
+#define PWM_GREEN D3
 void setup() {
     pinMode(LED, OUTPUT);
     digitalWrite(LED, LOW);
+
+    allServos.attach(PWM_RED, 1000, 2000);
+    allServos.attach(PWM_YELLOW, 1000, 2000);
+    allServos.attach(PWM_GREEN, 1000, 2000);
 }
 
 void ledON() {
@@ -16,9 +26,13 @@ void ledOFF() {
 
 void loop() {
     ledON();
-    delay(200);
+    delay(100);
 
     ledOFF();
-    delay(200);
+    delay(100);
+
+    allServos.write(PWM_RED, 0);
+    allServos.write(PWM_YELLOW, 90);
+    allServos.write(PWM_GREEN, 180);
 }
 
